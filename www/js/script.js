5a785263e5a1fb90b131203e1114a237c1349a32
Michael Rose <michael_rose@outlook.com>
2016-02-28 00:20:13 -0500

    Fix event binding and format time

--- www/js/script.js
+++ www/js/script.js
@@ -10,64 +10,94 @@
     var $counter = $('#counter');
     var $counterDisplay = $('#counterDisplay');
     var $btnTime = $('#btnTime');
-    var $btnBack = $('#btnBack')
+    var $btnStart = $('#btnStart')
     var $document = $(document);
+    var startTimeout = null;
+    var timer = new Timer(10);
 
-    $('#btnStart').on('click', function(event) {
+    $btnStart.on('click', function(event) {
       $puzzleName.text($puzzleDropdown.val());
       $choosePuzzle.hide();
       $timer.show();
+      $document.on('backbutton', back);
     });
 
-    $btnBack.on('click', function(event) {
+    $btnTime
+      .on('touchstart', ready)
+      .on('mousedown', ready)
+      .on('touchend', cancelReady)
+      .on('mouseup', cancelReady);
+
+    function back(event) {
       $timer.hide();
       $choosePuzzle.show();
-    });
+      $document.off('backbutton', back);
+    }
 
-    var startTimeout = null;
-
-    function timeStart(event) {
-      $counterDisplay.html('&nbsp;');
+    function ready(event) {
       startTimeout = setTimeout(function() {
         startTimeout = null;
-        $counter.css({ backgroundColor: 'green' });
-        $ready.css({ visibility: 'visible' });
+        $counter.hide();
+        $ready.css({
+          display: 'block',
+          visibility: 'visible',
+          backgroundColor: 'green'
+        });
+        $btnTime
+          .on('touchend', start)
+          .on('mouseup', start);
       }, 500);
     }
 
-    function timeEnd(event) {
+    function cancelReady(event) {
       if(startTimeout) {
         clearTimeout(startTimeout);
-      } else {
-        $btnTime.hide();
-        $btnBack.hide();
-        $counter.css({ backgroundColor: 'white' });
-        $ready.css({ visibility: 'hidden' });
-        var timer = new Timer(10);
-        var subscriber = function(ticks) {
-          $counterDisplay.text(ticks);
-        };
-        timer.subscribe(subscriber);
-        var endEvent = function(event) {
-          $document
-            .off('touchstart', endEvent)
-            .off('mousedown', endEvent);
-          timer.stop();
-          timer.unsubscribe(subscriber);
-          $btnTime.show();
-          $btnBack.show();
-        };
-        $document
-          .on('touchstart', endEvent)
-          .on('mousedown', endEvent);
-        timer.start();
       }
     }
 
-    $btnTime
-      .on('touchstart', timeStart)
-      .on('mousedown', timeStart)
-      .on('touchend', timeEnd)
-      .on('mouseup', timeEnd);
+    function start(event) {
+      $btnTime
+        .off('touchend', start)
+        .off('mouseup', start);
+      $btnTime.hide();
+      $counter.show();
+      $ready.hide();
+      timer.reset();
+      timer.subscribe(subscriber);
+      $document
+        .on('touchstart', done)
+        .on('mousedown', done);
+      timer.start();
+    }
+
+    function subscriber(ticks) {
+      $counterDisplay.text(formatTime(ticks));
+    }
+
+    function done(event) {
+      $document
+        .off('touchstart', done)
+        .off('mousedown', done);
+      timer.stop();
+      timer.unsubscribe(subscriber);
+      $btnTime.show();
+    }
+
+    function pad(num, size) {
+      var str = num.toString();
+      while(str.length < size) {
+        str = '0' + str;
+      }
+      return str;
+    }
+
+    function formatTime(ms) {
+      var mins = Math.floor(ms / 60000);
+      ms %= 60000;
+      var secs = Math.floor(ms / 1000);
+      ms %= 1000;
+      var subsecs = ms;
+      return mins + ':' + pad(secs, 2) + '.' + pad(subsecs, 3);
+    }
   });
 })(jQuery);
